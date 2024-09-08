41e7ddd918f87d9cf13388424583c723e69c4cf5
Jose Chiru F. <chirujavier12@gmail.com>
2024-09-08 09:42:42 -0500

    Se crea forma Suma Vector

---
+++ EjemploModuli1/FrmSumaVector.Designer.cs
@@ -0,0 +1,130 @@
+﻿namespace EjemploModuli1
+{
+    partial class FrmSumaVector
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.btnCalcular = new System.Windows.Forms.Button();
+            this.lstNumeros = new System.Windows.Forms.ListBox();
+            this.txtNum = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.btnCalcular);
+            this.panel1.Controls.Add(this.lstNumeros);
+            this.panel1.Controls.Add(this.txtNum);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Location = new System.Drawing.Point(157, 28);
+            this.panel1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(635, 417);
+            this.panel1.TabIndex = 3;
+            // 
+            // btnCalcular
+            // 
+            this.btnCalcular.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCalcular.Location = new System.Drawing.Point(275, 358);
+            this.btnCalcular.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.btnCalcular.Name = "btnCalcular";
+            this.btnCalcular.Size = new System.Drawing.Size(109, 47);
+            this.btnCalcular.TabIndex = 3;
+            this.btnCalcular.Text = "Calcular";
+            this.btnCalcular.UseVisualStyleBackColor = true;
+            this.btnCalcular.Click += new System.EventHandler(this.btnCalcular_Click);
+            // 
+            // lstNumeros
+            // 
+            this.lstNumeros.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lstNumeros.FormattingEnabled = true;
+            this.lstNumeros.ItemHeight = 31;
+            this.lstNumeros.Location = new System.Drawing.Point(108, 75);
+            this.lstNumeros.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.lstNumeros.Name = "lstNumeros";
+            this.lstNumeros.Size = new System.Drawing.Size(433, 252);
+            this.lstNumeros.TabIndex = 2;
+            // 
+            // txtNum
+            // 
+            this.txtNum.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtNum.Location = new System.Drawing.Point(275, 14);
+            this.txtNum.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.txtNum.Name = "txtNum";
+            this.txtNum.Size = new System.Drawing.Size(110, 34);
+            this.txtNum.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(123, 18);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(118, 31);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "Número:";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 22F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(150, -18);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(578, 42);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Suma de Números en un Vector\r\n";
+            // 
+            // FrmSumaVector
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ClientSize = new System.Drawing.Size(1003, 429);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.label1);
+            this.Name = "FrmSumaVector";
+            this.Text = "FrmSumaVector";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button btnCalcular;
+        private System.Windows.Forms.ListBox lstNumeros;
+        private System.Windows.Forms.TextBox txtNum;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+    }
+}
---
+++ EjemploModuli1/FrmSumaVector.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Microsoft.VisualBasic;
+
+namespace EjemploModuli1
+{
+    public partial class FrmSumaVector : Form
+    {
+        public FrmSumaVector()
+        {
+            InitializeComponent();
+        }
+
+        private void btnCalcular_Click(object sender, EventArgs e)
+        {
+            int cantidad = 0, contador = 0, numero;
+            int[] vectorNum;
+            lstNumeros.Items.Clear();
+            if (int.TryParse(txtNum.Text, out cantidad))
+            {
+                vectorNum = new int[cantidad];
+                while (cantidad > contador)
+                {
+                    string input = Microsoft.VisualBasic.Interaction.InputBox($"Ingrese el {contador + 1} valor", "Cargar Números");
+                    if (int.TryParse(input, out numero))
+                    {
+                        vectorNum[contador] = numero;
+                        lstNumeros.Items.Add(numero);
+                        contador++;
+                    }
+                    else
+                    {
+                        MessageBox.Show("Ingrese un valo númerico",
+                                            "Error",
+                                            MessageBoxButtons.OK,
+                                            MessageBoxIcon.Error);
+                    }
+
+                }
+                lstNumeros.Items.Add($"Sumatoria => {sumarNumeros(vectorNum)}");
+            }
+            else
+            {
+                MessageBox.Show("Ingrese un valo númerico",
+                    "Error",
+                    MessageBoxButtons.OK,
+                    MessageBoxIcon.Error);
+            }
+        }
+
+        private int sumarNumeros(int[] vectorNum)
+        {
+            int suma = 0;
+            foreach (int item in vectorNum)
+            {
+                suma += item;
+            }
+            return suma;
+        }
+
+    }
+}
